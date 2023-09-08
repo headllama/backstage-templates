9bf82ec08df870d36b27c8eda213a960eb108047
Renan Alves Medina <alvesrenansantos@hotmail.com>
2023-09-08 19:10:17 -0300

    update

--- terraform/vm/main.tf
+++ terraform/vm/main.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "{{values.machine_image}}"
+      image = "${{values.machine_image}}"
     }
   }
 
