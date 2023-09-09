8539a51c268443ffc41a7b230d9d06ea877ea0bb
Renan Alves Medina <alvesrenansantos@hotmail.com>
2023-09-08 22:28:45 -0300

    update templates

--- java/skeleton/src/main/java/com/example/template/TemplateApplication.java
+++ java/skeleton/src/main/java/com/example/template/TemplateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class "${{values.app_name}}"Application {
+public class ${{values.app_name}}Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run("${{values.app_name}}"Application.class, args);
+		SpringApplication.run(${{values.app_name}}Application.class, args);
 	}
 
 }
--- java/skeleton/src/test/java/com/example/template/TemplateApplicationTests.java
+++ java/skeleton/src/test/java/com/example/template/TemplateApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class "${{values.app_name}}"ApplicationTests {
+class ${{values.app_name}}ApplicationTests {
 
 	@Test
 	void contextLoads() {
