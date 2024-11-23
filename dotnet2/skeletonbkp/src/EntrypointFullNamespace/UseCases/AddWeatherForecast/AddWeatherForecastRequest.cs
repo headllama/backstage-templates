4a5484461b98601d5ff3d2f3013ef3bfb186c8e0
walber <walberhugo07@hotmail.com>
2024-11-23 19:07:00 -0300

    add pipeline teste

--- dotnet2/skeletonbkp/src/EntrypointFullNamespace/UseCases/AddWeatherForecast/AddWeatherForecastRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace EntrypointFullNamespace.UseCases.AddWeatherForecast;
-
-public class AddWeatherForecastRequest
-{
-    public DateTime? Date { get; set; }
-    public string Summary { get; set; }
-}
